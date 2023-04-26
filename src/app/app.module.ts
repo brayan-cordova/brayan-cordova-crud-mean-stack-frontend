73c501110e53aae981bbb77a8b5376019b978122
brayan-cordova <63522101+brayan-cordova@users.noreply.github.com>
2023-04-25 20:26:05 -0600

    installing bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentCrudComponent } from './student-crud/student-crud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentCrudComponent
   ],
   imports: [
     BrowserModule,
